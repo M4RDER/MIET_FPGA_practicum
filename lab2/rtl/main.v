84dac2a27fbb27801aa93902b532bd332db0f462
M4RDER <61751844+M4RDER@users.noreply.github.com>
2020-05-13 23:04:35 +0300

    Исправлен активный уровень одной из кнопок

--- lab2/rtl/main.v
+++ lab2/rtl/main.v
@@ -4,11 +4,13 @@
 
 module main(
   input            clk_50MHZ,
-  input      [9:0] SW,
-  input      [1:0] KEY,
-  output reg [6:0] HEX1,
-  output reg [6:0] HEX2,
-  output     [9:0] LedR
+  
+  input      [9:0] SW_i,
+  input      [1:0] KEY_i,
+  
+  output reg [6:0] HEX1_o,
+  output reg [6:0] HEX2_o,
+  output     [9:0] LedR_o
   );
   
   localparam CLK_FREQ_MHZ   = 50;
@@ -19,61 +21,61 @@ module main(
   
   registr u1(
     .clk_i  ( clk_50MHZ ),
-    .data_i ( SW        ),
-    .rstn_i ( KEY[1]    ),
-    .en_i   ( KEY[0]    ),
+    .data_i ( SW_i      ),
+    .rstn_i ( KEY_i[1]  ),
+    .en_i   ( KEY_i[0]  ),
     .data_o ( data      )
   ); 
   
   counter u2(
-    .SW        ( SW        ),
+    .SW_i      ( SW_i      ),
     .clk_i     ( clk_50MHZ ),
-    .rstn_i    ( KEY[1]    ),
-    .en_i      ( KEY[0]    ),
+    .rstn_i    ( KEY_i[1]  ),
+    .en_i      ( KEY_i[0]  ),
     .counter_o ( hex[7:0]  )
   );
   
   always@( posedge clk_50MHZ ) begin
-    case (hex[3:0])
-      4'd0  : HEX1 = 7'b100_0000;
-      4'd1  : HEX1 = 7'b111_1001;
-      4'd2  : HEX1 = 7'b010_0100;
-      4'd3  : HEX1 = 7'b011_0000;
-      4'd4  : HEX1 = 7'b001_1001;
-      4'd5  : HEX1 = 7'b001_0010;
-      4'd6  : HEX1 = 7'b000_0010;
-      4'd7  : HEX1 = 7'b111_1000;
-      4'd8  : HEX1 = 7'b000_0000;
-      4'd9  : HEX1 = 7'b001_0000;
-      4'd10 : HEX1 = 7'b000_1000;
-      4'd11 : HEX1 = 7'b000_0011;
-      4'd12 : HEX1 = 7'b100_0110;
-      4'd13 : HEX1 = 7'b010_0001;
-      4'd14 : HEX1 = 7'b000_0110;
-      4'd15 : HEX1 = 7'b000_1110;
+    case ( hex[3:0] )
+      4'd0  : HEX1_o = 7'b100_0000;
+      4'd1  : HEX1_o = 7'b111_1001;
+      4'd2  : HEX1_o = 7'b010_0100;
+      4'd3  : HEX1_o = 7'b011_0000;
+      4'd4  : HEX1_o = 7'b001_1001;
+      4'd5  : HEX1_o = 7'b001_0010;
+      4'd6  : HEX1_o = 7'b000_0010;
+      4'd7  : HEX1_o = 7'b111_1000;
+      4'd8  : HEX1_o = 7'b000_0000;
+      4'd9  : HEX1_o = 7'b001_0000;
+      4'd10 : HEX1_o = 7'b000_1000;
+      4'd11 : HEX1_o = 7'b000_0011;
+      4'd12 : HEX1_o = 7'b100_0110;
+      4'd13 : HEX1_o = 7'b010_0001;
+      4'd14 : HEX1_o = 7'b000_0110;
+      4'd15 : HEX1_o = 7'b000_1110;
     endcase
     
-    case (hex[7:4])
-      4'd0  : HEX2 = 7'b100_0000;
-      4'd1  : HEX2 = 7'b111_1001;
-      4'd2  : HEX2 = 7'b010_0100;
-      4'd3  : HEX2 = 7'b011_0000;
-      4'd4  : HEX2 = 7'b001_1001;
-      4'd5  : HEX2 = 7'b001_0010;
-      4'd6  : HEX2 = 7'b000_0010;
-      4'd7  : HEX2 = 7'b111_1000;
-      4'd8  : HEX2 = 7'b000_0000;
-      4'd9  : HEX2 = 7'b001_0000;
-      4'd10 : HEX2 = 7'b000_1000;
-      4'd11 : HEX2 = 7'b000_0011;
-      4'd12 : HEX2 = 7'b100_0110;
-      4'd13 : HEX2 = 7'b010_0001;
-      4'd14 : HEX2 = 7'b000_0110;
-      4'd15 : HEX2 = 7'b000_1110;
+    case ( hex[7:4] )
+      4'd0  : HEX2_o = 7'b100_0000;
+      4'd1  : HEX2_o = 7'b111_1001;
+      4'd2  : HEX2_o = 7'b010_0100;
+      4'd3  : HEX2_o = 7'b011_0000;
+      4'd4  : HEX2_o = 7'b001_1001;
+      4'd5  : HEX2_o = 7'b001_0010;
+      4'd6  : HEX2_o = 7'b000_0010;
+      4'd7  : HEX2_o = 7'b111_1000;
+      4'd8  : HEX2_o = 7'b000_0000;
+      4'd9  : HEX2_o = 7'b001_0000;
+      4'd10 : HEX2_o = 7'b000_1000;
+      4'd11 : HEX2_o = 7'b000_0011;
+      4'd12 : HEX2_o = 7'b100_0110;
+      4'd13 : HEX2_o = 7'b010_0001;
+      4'd14 : HEX2_o = 7'b000_0110;
+      4'd15 : HEX2_o = 7'b000_1110;
     endcase
   end
   
-  assign LedR = data;
+  assign LedR_o = data;
   
   
 endmodule
--- lab2/rtl/registr.v
+++ lab2/rtl/registr.v
@@ -2,9 +2,11 @@
 
 module registr(
   input        clk_i ,
+  
   input        rstn_i ,
   input        en_i,
   input  [9:0] data_i,
+  
   output [9:0] data_o 
   );
   
@@ -13,7 +15,7 @@ module registr(
   always@( posedge clk_i or negedge rstn_i ) begin
     if ( !rstn_i ) 
       data <= 0;
-    else if ( en_i )
+    else if ( !en_i )
       data <= data_i;
   end
   
